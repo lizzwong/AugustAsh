71e66e836e2ca91eebb1de1876ac01b747c50b23
Elizabeth Wong <lizzwong@gmail.com>
2018-05-21 10:29:32 -0500

    base started up

---
+++ server/public/scripts/client.js
@@ -0,0 +1,6 @@
+var myApp = angular.module('myApp', []); 
+
+
+myApp.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+}])
---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const PORT = process.env.PORT || 8888;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('server/public'));
+
+// const router = require('./routers/router')
+
+// app.use('/pictures', router);
+
+app.listen(PORT, function () {
+    console.log('Listening on port:', PORT);
+
+})
